b97d9104578a2e3a17a13573c5778085c6894750
heyongqi10 <heyongqi10@jd.com>
2026-09-09 09:38:17 +0800

    Add allowed development origins in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['jdcleaning-man-web-test.web.jdtest.net']
 };
 
 export default nextConfig;
